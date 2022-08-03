a173fc4e278f2e190dffec8a9cd6efb7bc2d6721
AjabiOladipupo <dipoajabisam95@gmail.com>
2022-08-03 20:01:13 +0100

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,58 @@
-#!/usr/bin/python3
-
-"""docstrings for base_model"""
-
+#!/usr/bin/pyhon3
+"""
+Parent class that will inherit
+"""
 import uuid
-from datetime import datetime, date
+from datetime import datetime
+from models import storage
 
 
-class BaseModel(object):
-    """BaseModel class defines all common
-    attributes/methods for other classes"""
-    def __init__(self):
-        """initialization function"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+class BaseModel:
+    """Defines all common attributes/methods
+    """
+    def __init__(self, *args, **kwargs):
+        """initializes all attributes
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
-        """specially formatted string represntation"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """returns class name, id and attribute dictionary
+        """
+        class_name = "[" + self.__class__.__name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
 
     def save(self):
-        """updates the 'updated_at' attribute"""
+        """updates last update time
+        """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """dictionary representation of object"""
-        return_value = {}
-        r = return_value    # Aliasing 'return_value'
-        for k, v in self.__dict__.items():
-            return_value[k] = v
-        return_value['__class__'] = self.__class__.__name__
-        r['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        r['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return return_value
+        """creates a new dictionary, adding a key and returning
+        datemtimes converted to strings
+        """
+        new_dict = {}
+
+        for key, values in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
+
+        return new_dict
